080eb9b1abdfe0b1e39a03450e5086513ceeb908
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2025-03-27 00:35:08 +0000

    Replace routes with resources shortcut

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,7 @@
 Rails.application.routes.draw do
   # Routes for the Movie resource:
 
-  # CREATE
-  post("/movies", { :controller => "movies", :action => "create" })
-          
-  # READ
-  get("/movies", { :controller => "movies", :action => "index" })
-  
-  get("/movies/:id", { :controller => "movies", :action => "show" })
-  
-  # UPDATE
-  
-  patch("/movies/:id", { :controller => "movies", :action => "update" })
-  
-  # DELETE
-  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+  resources :movies
 
   #------------------------------
 
